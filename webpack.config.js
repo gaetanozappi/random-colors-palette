f9f538e71b4e0f65bb1645a77cfe151bf0b7a7b3
gaetanozappi <gaetano.zappala.94@gmail.com>
2021-05-01 15:18:51 +0200

    Add babel, webpack config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"
+            }
+        ],
+    },
+};
